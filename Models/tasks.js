b0e9cf6c66377088460b2452ca39aba35fc3cd8d
Chun Yin Ho Wesley <choo0014@student.monash.edu>
2019-09-06 16:40:23 +1000

    schemas

---
+++ Models/tasks.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+let tasksSchema = mongoose.Schema({
+
+    _id: mongoose.Schema.Types.ObjectId,
+    taskName: {
+        type: String,
+        required: true
+    },
+    taskAssignTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'AssignTo'
+    },
+    taskDueDate: {
+        type: Date,
+        required: true
+    },
+    taskStatus: {
+        type: String,
+        validate: {
+            validator: function (statusVal) {
+                return statusVal === "inProgress" || statusVal === "Complete";
+            }
+        },
+        required: true
+    },
+    taskDesc: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Task', tasksSchema);
